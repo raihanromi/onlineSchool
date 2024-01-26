4e30cc611c9379effabb93812ac3969a1755348f
Raihan <98462487+raihanromi@users.noreply.github.com>
2024-01-26 19:51:54 +0600

    Delete onlineSchool directory

--- onlineSchool/js/course.js
+++
@@ -1,8 +0,0 @@
-function showCreateCourse() {
-    		var T = document.getElementById("createCourse");
-    		T.style.display = "block";
-		}
-		function hideCreateCourse() {
-    		var T = document.getElementById("createCourse");
-    		T.style.display = "none";
-		}
